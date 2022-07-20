8c73a62facdff42a318dd86fe956bf30c76bf115
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2022-07-20 17:48:56 +0300

    Add FastId; Add O(1) text nodes comparison; Change attr diff mode

--- core/src/test/scala/levsha/Change.scala
+++ core/src/test/scala/levsha/Change.scala
@@ -15,20 +15,20 @@ object Change {
 
   final class DiffTestChangesPerformer extends ChangesPerformer {
     private val buffer = mutable.Buffer.empty[Change]
-    def removeAttr(id: Id, xmlNs: String, name: String): Unit =
-      buffer += Change.removeAttr(id.toList.map(_.toInt), xmlNs: String, name)
-    def removeStyle(id: Id, name: String): Unit =
-      buffer += Change.removeStyle(id.toList.map(_.toInt), name)
-    def remove(id: Id): Unit =
-      buffer += Change.remove(id.toList.map(_.toInt))
-    def setAttr(id: Id, xmlNs: String, name: String, value: String): Unit =
-      buffer += Change.setAttr(id.toList.map(_.toInt), name, xmlNs, value)
-    def setStyle(id: Id, name: String, value: String): Unit =
-      buffer += Change.setStyle(id.toList.map(_.toInt), name, value)
-    def createText(id: Id, text: String): Unit =
-      buffer += Change.createText(id.toList.map(_.toInt), text)
-    def create(id: Id, xmlNs: String, tag: String): Unit =
-      buffer += Change.create(id.toList.map(_.toInt), tag, xmlNs)
+    def removeAttr(id: FastId, xmlNs: String, name: String): Unit =
+      buffer += Change.removeAttr(id.mkId.toList.map(_.toInt), xmlNs: String, name)
+    def removeStyle(id: FastId, name: String): Unit =
+      buffer += Change.removeStyle(id.mkId.toList.map(_.toInt), name)
+    def remove(id: FastId): Unit =
+      buffer += Change.remove(id.mkId.toList.map(_.toInt))
+    def setAttr(id: FastId, xmlNs: String, name: String, value: String): Unit =
+      buffer += Change.setAttr(id.mkId.toList.map(_.toInt), name, xmlNs, value)
+    def setStyle(id: FastId, name: String, value: String): Unit =
+      buffer += Change.setStyle(id.mkId.toList.map(_.toInt), name, value)
+    def createText(id: FastId, text: String): Unit =
+      buffer += Change.createText(id.mkId.toList.map(_.toInt), text)
+    def create(id: FastId, xmlNs: String, tag: String): Unit =
+      buffer += Change.create(id.mkId.toList.map(_.toInt), tag, xmlNs)
     def result: Seq[Change] = buffer.toVector
   }
 
